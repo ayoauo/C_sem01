f3301c208fbff6a3178f2b8074664a66cd4aa5ec
Ayo <ayo.ayo.ae@gmail.com>
2022-09-22 18:49:21 +0300

    Ayo003_game_secret number added

---
+++ Ayo003_game_secret_number/Program.cs
@@ -0,0 +1,39 @@
+﻿System.Console.WriteLine("Мы попали в игру угадай число");
+int secret_number = new Random().Next(1,11);
+int attempt = 5;
+bool Win = false;
+
+while (attempt > 0)
+{
+    Console.WriteLine("Введите число от 1 до 10");
+    int user_number = int.Parse(Console.ReadLine());
+    Console.WriteLine(user_number);
+    if(user_number > secret_number) 
+    {
+        Console.WriteLine("Секретное число меньше");
+        attempt--;
+    }
+    else if(user_number < secret_number)
+    {
+        Console.WriteLine("Секретное число больше");
+        attempt--;
+    }
+    else if(user_number == secret_number) // = присвоить == сравнить
+    {
+        Console.WriteLine("Вы угадали число");
+        Win = true;
+        break;
+    }
+    Console.WriteLine("Попробуй еще раз, попыток осталось " + attempt);
+}
+if(Win)
+{
+    Console.WriteLine("Вы победили, увидимся через неделю");
+    Console.WriteLine("Осталось попыток " + (attempt-1));
+}else
+{
+     Console.WriteLine("Вы проиграли, секретное число было " + secret_number);
+     Console.WriteLine("Попробуйте еще раз");
+}
+
+
