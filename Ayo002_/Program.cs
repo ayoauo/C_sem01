e35f5265677e964a21c740a53548eebdddfdf5e8
Ayo <ayo.ayo.ae@gmail.com>
2022-09-22 17:20:07 +0300

    Ayo002_added

---
+++ Ayo002_/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = 12; // -999 999
+
+Console.WriteLine("Месяцев в году " + number);
+string n = number.ToString();
+
+Console.WriteLine("Введите ваше имя");
+string name = Console.ReadLine(); // Отдает все только строкой
+Console.WriteLine("Привет, " + name);
+Console.WriteLine("Сколько вам лет?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Вам " + age + " лет");
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a + b);
+
+if(a > b)
+{
+    Console.WriteLine("Число а больше числа b");
+}else if(a < b)
+{
+    Console.WriteLine("Число b больше числа a");
+}else
+{
+    Console.WriteLine("Число a равно числу b");
+}
