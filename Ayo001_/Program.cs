9e52a8a71f82d47ecad68949951b771663be6b1b
Ayo <ayo.ayo.ae@gmail.com>
2022-09-22 15:42:40 +0300

    Info about variables added

--- Ayo001_/Program.cs
+++ Ayo001_/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 12; // -999 999
+string name = "Привет, Вася";
+float x = 5.1f; // -999.54
+
+bool flag = true; // false
+double b = 34.65;
+char type = 'b';
+
+var r = "asdaf"; // Можно хранить все
